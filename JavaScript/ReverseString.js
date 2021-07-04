4b6f23360b13b695fc463fffa899da953d85b8a9
Jéssika Sousa <84594383+jessikasousa@users.noreply.github.com>
2021-07-04 18:55:51 -0300

    Create ReverseString.js

---
+++ JavaScript/ReverseString.js
@@ -0,0 +1,13 @@
+//Reverse String
+
+let newStr = '';
+
+function reverseString(str) {
+    for (let i = str.length - 1; i >= 0; i--) {
+        newStr += str[i]
+        console.log(newStr);
+    }
+}
+
+
+let resultado = reverseString('Hello World!')
